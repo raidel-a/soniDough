12dfc1d6e883735193659b47cfdb894c3e9d2c80
raihven <raihven@protonmail.com>
2023-12-26 18:54:00 -0500

    Fix UI issues and add new components

---
+++ app/profile/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { AuthSession } from "@supabase/auth-ui-react/dist/components/Auth/UserContext";
+import 
+
+// interface profileProps {
+//   user:;
+// }
+
+
+const profile = () => {
+  return <div></div>;
+};
+
+export default profile;
