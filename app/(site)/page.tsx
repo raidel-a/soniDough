07c1fc14f2b33a768203a26bb34737ba62a1c857
Raidel A <15664418+raidel-a@users.noreply.github.com>
2024-10-17 04:10:30 -0400

    use supabase auth

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,32 +2,40 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 import getSongs from "@/actions/getSongs";
 import PageContent from "./components/PageContent";
-import { getServerSession } from "next-auth/next";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 export const revalidate = 0;
 
-async function getData() {
-  const session = await getServerSession();
-  let songs = null;
-  if (session?.user) {
-    songs = await getSongs();
-  }
-  return { songs, user: session?.user };
-}
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  
+  const { data: { session } } = await supabase.auth.getSession();
 
-export default async function Page() {
-  const { songs, user } = await getData();
+  const songs = await getSongs();
 
   return (
-    <div>
-      {user ? (
-        <div>
-          <h1>Welcome, {user.name}!</h1>
-          {songs && <PageContent songs={songs} />}
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">
+            Welcome {session?.user?.email}
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem 
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
+          </div>
         </div>
-      ) : (
-        <h1>Please log in to see your songs.</h1>
-      )}
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+        </div>
+        <PageContent songs={songs} />
+      </div>
     </div>
   );
 }
